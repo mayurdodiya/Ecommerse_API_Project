f701b463ccb601df8fb9e240b9d893f764dd5d64
mayurdodiya <132254613+mayurdodiya@users.noreply.github.com>
2023-05-10 09:54:41 +0530

    Mayur Dodiya

--- controller/saller_controller.js
+++ controller/saller_controller.js
@@ -22,7 +22,7 @@ var add_saller = async (req, res) => {
     var data = await Permission_pending_schema.create(req.body);
 
     res.status(200).json({
-      status: "Sign in Success",
+      status: "Your acc. create succesfully plz wait for admin permission",
       data
     })
   } else {
